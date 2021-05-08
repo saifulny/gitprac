dac3a14867a13077f7e6f18003c6e2cd19e04321
Saiful <saiful@gmail.com>
2021-05-08 14:13:14 -0400

    changed

--- MaternityProject/src/test/java/com/maternity/test/BaseTest.java
+++ MaternityProject/src/test/java/com/maternity/test/BaseTest.java
@@ -21,7 +21,7 @@ public class BaseTest {
 
 	@BeforeMethod
 	public void openBrowser() throws IOException {
-     System.out.println("Opened Browser effectively");
+     System.out.println("Opened Browser now");
      
 		FileInput fi = new FileInput();
 
