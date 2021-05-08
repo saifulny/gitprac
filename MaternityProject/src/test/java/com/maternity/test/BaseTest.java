9bea2bff9945659462556bacd686ff8cfb28ad77
Saiful <saiful@gmail.com>
2021-05-08 13:56:56 -0400

    update

--- MaternityProject/src/test/java/com/maternity/test/BaseTest.java
+++ MaternityProject/src/test/java/com/maternity/test/BaseTest.java
@@ -21,7 +21,8 @@ public class BaseTest {
 
 	@BeforeMethod
 	public void openBrowser() throws IOException {
-     System.out.println("Open Browser");
+     System.out.println("Opened Browser effectively");
+     
 		FileInput fi = new FileInput();
 
 		pr = fi.readFile();
